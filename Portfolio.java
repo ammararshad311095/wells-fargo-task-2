f36b2cc8491545f1e4669e43b9614e6f7e4d012a
ammararshad311095 <ammararshad228@gmail.com>
2025-09-10 23:21:26 +0800

    Create Portfolio.java

---
+++ Portfolio.java
@@ -0,0 +1,43 @@
+package com.wellsfargo.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long portfolioId;
+
+    @Temporal(TemporalType.DATE)
+    private Date creationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId", nullable = false)
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Security> securities;
+
+    // Constructor
+    public Portfolio(Date creationDate, Client client) {
+        this.creationDate = creationDate;
+        this.client = client;
+    }
+
+    public Portfolio() {}
+
+    // Getters and Setters
+    public Long getPortfolioId() { return portfolioId; }
+
+    public Date getCreationDate() { return creationDate; }
+    public void setCreationDate(Date creationDate) { this.creationDate = creationDate; }
+
+    public Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client; }
+
+    public List<Security> getSecurities() { return securities; }
+    public void setSecurities(List<Security> securities) { this.securities = securities; }
+}
